8d6f9ad7b3b725cc7c7f7ecb4dd0f22f3f2a06b8
bangaffannn <bangaffannn@gmail.com>
2024-02-04 00:32:17 +0700

    Adding backend models, route, controllers

--- backend/index.js
+++ backend/index.js
@@ -3,4 +3,15 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 const app = express();
+mongoose.connect('mongodb://localhost:27017/fullstack_db',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Database Connected...'));
+
+app.use(cors());
+app.use(express.json());
+
 app.listen(5000, () => console.log("Server up and running..."));
