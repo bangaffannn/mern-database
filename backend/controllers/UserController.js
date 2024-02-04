621d3d7c695e2c9b2701a72df37639090c0ff5c6
bangaffannn <bangaffannn@gmail.com>
2024-02-04 11:25:01 +0700

    Updating backend and  creating frontend

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,4 @@
-import User from "../models/UserModel";
+import User from "../models/UserModel.js";
 
 export const getUsers = async (req, res) => {
   try {
@@ -6,5 +6,45 @@ export const getUsers = async (req, res) => {
     res.json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+export const getUsersById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const saveUser = async (req, res) => {
+  const user = new User(req.body);
+  try {
+    const inserteduser = await user.save();
+    res.status(201).json(inserteduser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const updateuser = await User.updateOne(
+      { _id: req.params.id },
+      { $set: req.body }
+    );
+    res.status(200).json(updateuser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const deleteduser = await User.deleteOne({ _id: req.params.id });
+    res.status(200).json(deleteduser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 };
